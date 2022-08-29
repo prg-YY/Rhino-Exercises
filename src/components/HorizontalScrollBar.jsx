4663aa50c1450d7fa76b14cd9e926266ba7ce779
prg0615 <parguzatt@gmail.com>
2022-08-29 21:07:10 +0200

    bug is fixed

--- src/components/HorizontalScrollBar.jsx
+++ src/components/HorizontalScrollBar.jsx
@@ -1,9 +1,8 @@
-import { useContext } from "react"
-
+import React, { useContext } from "react"
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu"
-import { Box, Typography } from "@mui/material"
-import BodyPart from "./BodyPart"
+import { Box, Typography, Button } from "@mui/material"
 
+import BodyPart from "./BodyPart"
 import RightArrowIcon from "../assets/icons/right-arrow.png"
 import LeftArrowIcon from "../assets/icons/left-arrow.png"
 
@@ -22,26 +21,23 @@ const RightArrow = () => {
 
   return (
     <Typography onClick={() => scrollNext()} className="left-arrow">
-      <img src={RightArrowIcon} alt="right-arrow" />
+      <img src={RightArrowIcon} alt="left-arrow" />
     </Typography>
   )
 }
 
-const HorizontalScrollBar = ({ data, bodyPart, setBodyPart }) => {
-  return (
-    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-      {data.map((item) => (
-        <Box
-          key={item.id || item}
-          itemID={item.id || item}
-          title={item.id || item}
-          m="0 40px"
-        >
-          <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
-        </Box>
-      ))}
-    </ScrollMenu>
-  )
-}
-
+const HorizontalScrollBar = ({ data, bodyPart, setBodyPart }) => (
+  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+    {data.map((item) => (
+      <Box
+        key={item.id || item}
+        itemID={item.id || item}
+        title={item.id || item}
+        m="0 40px"
+      >
+        <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} />
+      </Box>
+    ))}
+  </ScrollMenu>
+)
 export default HorizontalScrollBar
--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -49,7 +49,7 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
           height="76px"
           sx={{
             input: { fontWeight: "700", border: "none", borderRadius: "4px" },
-            width: { lg: "1170px", xs: "350px" },
+            width: { lg: "1170px", md: "768px", xs: "350px" },
             backgroundColor: "#fff",
             borderRadius: "40px",
           }}
@@ -75,7 +75,10 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
           Search
         </Button>
       </Box>
-      <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
+      <Box
+        component="div"
+        sx={{ position: "relative", width: "100%", p: "20px" }}
+      >
         <HorizontalScrollBar
           data={bodyParts}
           bodyPart={bodyPart}
