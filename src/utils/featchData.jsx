4d7720197bec1a9c60e52922fd7fbf061fb282da
prg0615 <parguzatt@gmail.com>
2022-08-30 22:46:29 +0200

    exercise-card is done

--- src/utils/featchData.jsx
+++ src/utils/featchData.jsx
@@ -3,6 +3,9 @@ export const BASE_URL =
 
 export const exerciseOptions = {
   method: "GET",
+  params: {
+    maxResults: 50,
+  },
   headers: {
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
     "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
