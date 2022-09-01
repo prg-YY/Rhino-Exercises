3aa738087e58b1f1bd7027f4e1f25b7ff127a938
prg0615 <parguzatt@gmail.com>
2022-09-01 20:44:42 +0200

    got api error

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -3,10 +3,48 @@ import React, { useState, useEffect } from "react"
 import Pagination from "@mui/material/Pagination"
 import { Box, Stack, Typography } from "@mui/material"
 
-import { exerciseOptions, featchData } from "../utils/featchData"
+import { exerciseOptions, fetchData } from "../utils/featchData"
 import ExerciseCard from "./ExerciseCard"
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  const [currentPage, setCurrentPage] = useState(1)
+  const [exercisesPerPage] = useState(6)
+
+  // Pagination
+  const indexOfLastExercise = currentPage * exercisesPerPage
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage
+  const currentExercises = exercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise
+  )
+
+  const paginate = (e, value) => {
+    setCurrentPage(value)
+
+    window.scrollTo({ top: 1800, behavior: "smooth" })
+  }
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = []
+
+      if (bodyPart === "all") {
+        exercisesData = await fetchData(
+          "https://exercisedb.p.rapidapi.com/exercises",
+          exerciseOptions
+        )
+      } else {
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        )
+      }
+
+      setExercises(exercisesData)
+    }
+
+    fetchExercisesData()
+  }, [bodyPart])
   return (
     <Box className="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
       <Typography variant="h4" mb="46px">
@@ -18,12 +56,22 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         flexWrap="wrap"
         justifyContent="center"
       >
-        {exercises.map((exercise, index) => (
+        {currentExercises.map((exercise, index) => (
           <ExerciseCard key={index} exercise={exercise} />
         ))}
       </Stack>
-      <Stack mt="100px" alignItems="center">
-        {exercises.length > 9 && <Pagination color="standard" />}
+      <Stack sx={{ mt: { lg: "114px", xs: "70px" } }} alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="medium"
+          />
+        )}
       </Stack>
     </Box>
   )
